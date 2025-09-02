6e4a0d85d2ac7e19f3f6abce573e5a206cf26d41
xtxn <atchuchev@gmail.com>
2025-09-03 00:27:18 +0300

    fixed modal functionality

--- movie-search/src/ui/components/modal.js
+++ movie-search/src/ui/components/modal.js
@@ -1,13 +1,21 @@
-// TODO Modal template
+import { getTrailerData } from "../../services/api.js";
 
 const modalTarget = document.getElementById('movie-modal');
 const fallbackImgSrc = '../../noposter1.png';
-let imdbID = '';
 
-export function openModal(movieInfo, ytID) {
-    // ytID = ytID;
-    imdbID = movieInfo.imdbID;
+export async function openModal(movieInfo, onFavClick) {
     modalTarget.replaceChildren("");
+
+    let ytID = null;
+    try {
+        const trailersData = await getTrailerData(movieInfo.imdbID);
+        if (!trailersData.message?.includes("No trailers found")) {
+            ytID = trailersData[0]?.youtube_video_id;
+        }
+    } catch (error) {
+        console.warn('Could not fetch trailer data.');
+    }
+
     const divContent = document.createElement('div');
     divContent.classList.add("modal-content");
     divContent.innerHTML = `
@@ -17,27 +25,27 @@ export function openModal(movieInfo, ytID) {
         <img img src=${movieInfo.Poster !== 'N/A' ? movieInfo.Poster : fallbackImgSrc
         } onerror="this.onerror=null; this.src='${fallbackImgSrc}';" alt = "${movieInfo.Title} Poster"  class="img-details img">
         <section class="modal-info">
-            <h2 class="modal-movie-title">${movieInfo.Title}</h2>
+            <h2 class="modal-movie-title">${movieInfo.Title}(${movieInfo.Year})</h2>
             <div class="movie-info">
                 <span class="imdb bubble">imdb: ${movieInfo.imdbRating}</span>
                 <span class="box-office bubble">BoxOffice: ${movieInfo.BoxOffice}</span>
-                <span class="type bubble">Type: movie</span>
+                <span class="type bubble">Type: ${movieInfo.Type}</span>
             </div>
             <p class="modal-movie-description">
                 ${movieInfo.Plot}
             </p>
             <p class="movie-info">
-                <button class="btn-trailer">Trailer<i class="fa-solid fa-play"></i>
-                </button>
-                <button class="btn-fav">Add<i class="fa-solid fa-heart"></i>
-                </button>
+                <button class="btn-trailer">Trailer<i class="fa-solid fa-play"></i></button>
+                <button class="btn-fav">Add<i class="fa-solid fa-heart"></i></button>
             </p>
         </div>
     </div>`
     modalTarget.appendChild(divContent);
 
     document.getElementById("modal-close-btn").addEventListener('click', closeModal);
-    document.querySelector(".btn-trailer").addEventListener('click', () => playTrailer(ytID))
+    document.querySelector(".btn-trailer").addEventListener('click', () => playTrailer(ytID));
+    document.querySelector(".btn-fav").addEventListener('click', onFavClick);
+
 }
 
 function closeModal() {
@@ -45,7 +53,10 @@ function closeModal() {
 }
 
 function playTrailer(ytID) {
-    if (ytID !== null) {
+    if (ytID) {
         window.open(`https://www.youtube.com/watch?v=${ytID}`, '_blank');
+    } else {
+        throw error;
     }
 }
+
